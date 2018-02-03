c982e8e9793ad525defcb83b1af98914f0287846
marsalad <buggymarsala@gmail.com>
2018-02-03 05:20:20 -0500

    clumping messages

--- processTexts.py
+++ processTexts.py
@@ -1,4 +1,5 @@
 import messengerScraper
+import datetime
 import json
 
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
@@ -6,7 +7,8 @@ from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions, KeywordsOptions
 
 watsonCreds = 'ibm-key.json'
-minInterval = 10
+clumpMins = 20
+endPunctuation = set(['!', ',', '.', ';', '?'])
 
 def initWatson():
 	with open(watsonCreds, 'r') as f:
@@ -16,6 +18,43 @@ def initWatson():
 		password = creds['password'],
 		version = '2017-02-27')
 
+def makeClumps(name, convos):
+	allClumps = []
+	userClumps = []
+	for convo in convos:
+		allMsgs = []
+		userMsgs = []
+		clumpEndTime = convo[0]['time'] + datetime.timedelta(minutes=clumpMins)
+		for msg in convo:
+			if msg['time'] > clumpEndTime:
+				if len(allMsgs) > 0:
+					allClumps.append(clumpMsgs(allMsgs))
+					allMsgs = []
+					if len(userMsgs) > 0:
+						userClumps.append(clumpMsgs(userMsgs))
+						userMsgs = []
+				clumpEndTime = msg['time'] + datetime.timedelta(minutes=clumpMins)
+			allMsgs.append(msg)
+			if msg['user'] == name:
+				userMsgs.append(msg)
+		if len(allMsgs) > 0:
+			allClumps.append(clumpMsgs(allMsgs))
+			if len(userMsgs) > 0:
+				userClumps.append(clumpMsgs(userMsgs))
+	return allClumps, userClumps
+
+def clumpMsgs(msgs):
+	clump = {'time': msgs[0]['time'], 'user': msgs[0]['user'], 'text': ''}
+	for msg in msgs:
+		clump['text'] += msg['text']
+		if msg['text'][-1:] not in endPunctuation:
+			clump['text'] += '. '
+		else:
+			clump['text'] += ' '
+	return clump
+
 if __name__ == "__main__":
-	natural_language_understanding = initWatson()
-	# name, msgs = messengerScraper.scrapeAll('data');
+	name, convos = messengerScraper.scrapeAll('data')
+	allClump, userClump = makeClumps(name, convos)
+	for clump in userClump:
+		print(clump)
