a7bc8c91706ef27849c2a981955ef5d8d7497fc9
marsalad <buggymarsala@gmail.com>
2018-02-03 05:34:26 -0500

    changed clump format

--- processTexts.py
+++ processTexts.py
@@ -19,29 +19,24 @@ def initWatson():
 		version = '2017-02-27')
 
 def makeClumps(name, convos):
-	allClumps = []
-	userClumps = []
+	clumped = []
 	for convo in convos:
 		allMsgs = []
 		userMsgs = []
 		clumpEndTime = convo[0]['time'] + datetime.timedelta(minutes=clumpMins)
 		for msg in convo:
 			if msg['time'] > clumpEndTime:
-				if len(allMsgs) > 0:
-					allClumps.append(clumpMsgs(allMsgs))
-					allMsgs = []
-					if len(userMsgs) > 0:
-						userClumps.append(clumpMsgs(userMsgs))
-						userMsgs = []
+				if len(userMsgs) > 0:
+					clumped.append((clumpMsgs(allMsgs), clumpMsgs(userMsgs)))
+				allMsgs = []
+				userMsgs = []
 				clumpEndTime = msg['time'] + datetime.timedelta(minutes=clumpMins)
 			allMsgs.append(msg)
 			if msg['user'] == name:
 				userMsgs.append(msg)
-		if len(allMsgs) > 0:
-			allClumps.append(clumpMsgs(allMsgs))
-			if len(userMsgs) > 0:
-				userClumps.append(clumpMsgs(userMsgs))
-	return allClumps, userClumps
+		if len(userMsgs) > 0:
+			clumped.append((clumpMsgs(allMsgs), clumpMsgs(userMsgs)))
+	return clumped
 
 def clumpMsgs(msgs):
 	clump = {'time': msgs[0]['time'], 'user': msgs[0]['user'], 'text': ''}
@@ -53,8 +48,11 @@ def clumpMsgs(msgs):
 			clump['text'] += ' '
 	return clump
 
+def sentimentAnalysis(msg):
+	pass
+
 if __name__ == "__main__":
 	name, convos = messengerScraper.scrapeAll('data')
-	allClump, userClump = makeClumps(name, convos)
-	for clump in userClump:
-		print(clump)
+	clumps = makeClumps(name, convos)
+	for clump in clumps:
+		print(clump[1])
